4eee176f6b87d220986e89a06f527c6799b39adc
Joe Edwards <josephlukeedwards@gmail.com>
2016-07-26 17:41:01 +0100

    I3i: game complete - needs refactoring with Sinatra settings

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-answer = rand(101)
+ANSWER = rand(101)
 
-get '/' do 
-	erb :index, :locals => {:answer => answer}
+
+get '/' do
+	guess = params["guess"]
+	message = check_guess(guess)
+	erb :index, :locals => {:message => message}
+end
+
+def check_guess(guess)
+	message = "Please make a guess"
+	if guess
+		guess = guess.to_i
+		if guess - 10 > ANSWER
+			message = "Way too high!"
+		elsif guess > ANSWER
+			message = "Too high!"
+		elsif guess + 10 < ANSWER
+			message = "Way too low!"
+		elsif guess < ANSWER
+			message = "Too low!"
+		else
+			message = "You got it right! The SECRET NUMBER is #{ANSWER}"
+		end
+	end
+	message
 end
