add87495dd605bd68e7d550e834673d67c61d7a9
Joe Edwards <josephlukeedwards@gmail.com>
2016-07-27 16:59:35 +0100

    I4i: background colour reflects guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,28 +3,59 @@ require 'sinatra/reloader'
 
 set :answer, rand(101)
 
-
 get '/' do
 	guess = params["guess"]
-	message = check_guess(guess)
-	erb :index, :locals => {:message => message}
+	comparison = check_guess(guess)
+	message = set_message(comparison)
+	background_color = set_color(comparison)
+	erb :index, :locals => {:message => message, :background_color => background_color}
 end
 
 def check_guess(guess)
-	message = "Please make a guess"
+	comparison = 3
 	if guess
 		guess = guess.to_i
 		if guess - 10 > settings.answer
-			message = "Way too high!"
+			comparison = 2
 		elsif guess > settings.answer
-			message = "Too high!"
+			comparison = 1
 		elsif guess + 10 < settings.answer
-			message = "Way too low!"
+			comparison = -2
 		elsif guess < settings.answer
-			message = "Too low!"
-		else
-			message = "You got it right! The SECRET NUMBER is #{settings.answer}"
+			comparison = -1		
+		elsif guess == settings.answer
+			comparison = 0
 		end
 	end
+	comparison
+end
+
+def set_message(comparison)
+	message = "Please make a guess"
+	case comparison
+	when 2
+		message = "Way too high!"
+	when 1
+		message = "Too high!"
+	when 0
+		message = "You got it right! The SECRET NUMBER is #{settings.answer}"
+	when -1
+		message = "Too low!"
+	when -2
+		message = "Way too low!"
+	end
 	message
+end
+
+def set_color(comparison)
+	color = 'white'
+	case comparison
+	when 2, -2
+		color = 'firebrick'
+	when 1, -1
+		color = 'lightpink'
+	when 0
+		color = 'mediumseagreen'
+	end
+	color
 end
